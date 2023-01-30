7062b24f7deae56440050cd1bc046b0596f6a9f6
nasimimran <nasimuzzamanimran@gmail.com>
2023-01-30 15:22:13 +0600

    Initial commit

---
+++ snake_game/src/GamePanel.java
@@ -0,0 +1,41 @@
+import javax.swing.JPanel;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+public class GamePanel extends JPanel implements AncestorListener {
+
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void ancestorAdded(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ancestorRemoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ancestorMoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
